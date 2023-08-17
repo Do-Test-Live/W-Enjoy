6c5b02b67b27c28e531c74287408ddbe25be1196
Mugdho <saha2171515@stud.kuet.ac.bd>
2023-08-17 18:57:35 +0600

    17/08/2023

--- config.php
+++ config.php
@@ -12,8 +12,8 @@ $currency = "hkd";
  * Remember to switch to your live publishable and secret key in production!
  * See your keys here: https://dashboard.stripe.com/account/apikeys
  */
-define('STRIPE_API_KEY', 'sk_test_51Js7yMEef0ZfRYif2EiZDwOsgckQWfhufIaCy6AA4V2khdgX3HjPVUVayrmgoaSZrbKWurDNaS51ykeUjVkDvH3C00jqJ2LxSf');
-define('STRIPE_PUBLISHABLE_KEY', 'pk_test_51Js7yMEef0ZfRYif2yukLvrLbqS7IhDndBYnLlxB6oFfD354C5GDXlrkoUfhR0ICChKmTrzeWi7sfKUvrQmXajXJ00hPvG6I62');
+define('STRIPE_API_KEY', 'sk_test_51NfHNYJNLfAYo4EuUD0ars7qIkvLDLmQtbv852XUBJRYu8XZyuW6VqT77bbJFarLu8V1k1CPOxcnJ0DUmdMcW5UB00xZXKVhK2');
+define('STRIPE_PUBLISHABLE_KEY', 'pk_test_51NfHNYJNLfAYo4EuQ68jCsooFIyQdR8lnl1OiiW2tDkbb3RimgrDem2Xi8OoDpbeF3HF7FuahpKPi2qowBykfX89001ryatLws');
 define('STRIPE_SUCCESS_URL', 'https://localhost/Enjoy/payment-success.php'); //Payment success URL
 define('STRIPE_CANCEL_URL', 'https://localhost/Enjoy/payment-cancel.php'); //Payment cancel URL
 
